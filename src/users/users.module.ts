e8f08eb7eb54cbde91753331ee2f7fa985894537
vickydarlinn <uttapalsangwan@gmail.com>
2025-06-14 13:58:07 +0530

    fix: hide users api endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+// import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
+  // controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
